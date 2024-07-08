1c57694d1e443275c9be6af0271f00ccc64d3809
JamesK-C <j.kavanaghcranston@gmail.com>
2024-07-09 00:50:21 +0100

    accelerometer.py

--- src/instruments/accelerometer.py
+++ src/instruments/accelerometer.py
@@ -2,3 +2,32 @@
 #
 # Datasheet:
 # https://www.mouser.co.uk/datasheet/2/744/en_DM00133076-2488588.pdf
+
+import time
+import board
+from adafruit_lsm6ds.lsm6ds3 import LSM6DS3
+
+def initilise_accelerometer():
+    """Function used to initialise the accelerometer module."""
+
+    i2c = board.I2C()
+    sensor = LSM6DS3(i2c)
+    return sensor
+
+def get_acceleration(sensor):
+    return sensor.acceleration
+
+def get_gyro(sensor):
+    return sensor.gyro
+
+def main():
+    while True:
+        # print("Acceleration: X:%.2f, Y: %.2f, Z: %.2f m/s^2" % (sensor.acceleration))
+        # print("Gyro X:%.2f, Y: %.2f, Z: %.2f radians/s" % (sensor.gyro))
+        print(get_acceleration())
+        print(get_gyro())
+        print("")
+        time.sleep(0.5)
+
+if __name__ == '__main__':
+    main()
