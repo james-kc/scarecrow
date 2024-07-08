205ab16b509643c7bddd4d7f61e59a9ae51043ad
JamesK-C <j.kavanaghcranston@gmail.com>
2024-07-09 00:44:52 +0100

    change to lib

--- library_examples/accelerometer/lsm6ds_lsmds3_simpletest.py
+++ library_examples/accelerometer/lsm6ds_lsmds3_simpletest.py
@@ -9,8 +9,14 @@ i2c = board.I2C()  # uses board.SCL and board.SDA
 # i2c = board.STEMMA_I2C()  # For using the built-in STEMMA QT connector on a microcontroller
 sensor = LSM6DS3(i2c)
 
-while True:
-    print("Acceleration: X:%.2f, Y: %.2f, Z: %.2f m/s^2" % (sensor.acceleration))
-    print("Gyro X:%.2f, Y: %.2f, Z: %.2f radians/s" % (sensor.gyro))
-    print("")
-    time.sleep(0.5)
+try:
+    while True:
+        # Print Acceleration
+        print("\rAcceleration: X:%.2f, Y: %.2f, Z: %.2f m/s^2" % (sensor.acceleration))
+        # Print Gyro
+        print("\rGyro X:%.2f, Y: %.2f, Z: %.2f radians/s" % (sensor.gyro))
+        # Sleep for 0.5 seconds
+        time.sleep(0.5)
+
+except KeyboardInterrupt:
+    print("\nProgram terminated by user")
