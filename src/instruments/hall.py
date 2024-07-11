e29f823082c7bf5596ad15cb7d75fce98362a90c
james-kc <78704331+james-kc@users.noreply.github.com>
2024-07-11 22:56:52 +0100

    changed serial port

--- src/instruments/hall.py
+++ src/instruments/hall.py
@@ -2,7 +2,7 @@ import serial
 import time
 
 # Setup serial connection
-ser = serial.Serial('/dev/ttyS0', 9600, timeout=1)
+ser = serial.Serial('/dev/ttyAMA0', 9600, timeout=1)
 
 while True:
     if ser.in_waiting > 0:
@@ -10,4 +10,6 @@ while True:
         sensor_data = ser.readline().decode('utf-8').strip()
         if sensor_data:
             print(f"Received: {sensor_data}")
+        else:
+            print("Got no milk")
     time.sleep(0.1)  # Adjust the delay as needed
