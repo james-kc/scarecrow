ff9d32a73c48a011823508b83a825fb003be3a5b
james-kc <78704331+james-kc@users.noreply.github.com>
2024-07-11 12:19:27 +0100

    transmitter lib and gps update

--- library_examples/gps/gps_echotest.py
+++ library_examples/gps/gps_echotest.py
@@ -11,25 +11,24 @@ import busio
 import adafruit_gps
 
 print("Imports complete.")
-quit()
 
 # Create a serial connection for the GPS connection using default speed and
 # a slightly higher timeout (GPS modules typically update once a second).
 # These are the defaults you should use for the GPS FeatherWing.
 # For other boards set RX = GPS module TX, and TX = GPS module RX pins.
-uart = busio.UART(board.TX, board.RX, baudrate=9600, timeout=10)
+# uart = busio.UART(board.TX, board.RX, baudrate=9600, timeout=10)
 
 # for a computer, use the pyserial library for uart access
 # import serial
 # uart = serial.Serial("/dev/ttyUSB0", baudrate=9600, timeout=10)
 
 # If using I2C, we'll create an I2C interface to talk to using default pins
-# i2c = board.I2C()  # uses board.SCL and board.SDA
+i2c = board.I2C()  # uses board.SCL and board.SDA
 # i2c = board.STEMMA_I2C()  # For using the built-in STEMMA QT connector on a microcontroller
 
 # Create a GPS module instance.
-gps = adafruit_gps.GPS(uart)  # Use UART/pyserial
-# gps = adafruit_gps.GPS_GtopI2C(i2c)  # Use I2C interface
+# gps = adafruit_gps.GPS(uart)  # Use UART/pyserial
+gps = adafruit_gps.GPS_GtopI2C(i2c)  # Use I2C interface
 
 # Initialize the GPS module by changing what data it sends and at what rate.
 # These are NMEA extensions for PMTK_314_SET_NMEA_OUTPUT and
